67ad4331e6d800320c97152b521b93a137a5c663
Arun-Misra <kanhaam2006@gmail.com>
2026-02-15 03:04:48 +0530

    test successfull

--- database.py
+++ database.py
@@ -5,7 +5,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-test
 db_url = os.getenv("POSTGRES")
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
